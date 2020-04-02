da8fda50a384f7d749c061103dc83e93c5cdbf4b
tomoaki.iwamoto <info@tokiwakanpou.com>
2020-04-02 10:37:32 +0900

    Initial Commit

---
+++ SlideshowApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideshowApp
+//
+//  Created by 岩本知章 on 2020/04/02.
+//  Copyright © 2020 tomoaki.iwamoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
