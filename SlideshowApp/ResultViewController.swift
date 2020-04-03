87376bb2cf4b8e0b0f5d0e33950020c6a96b6b7e
tomoaki.iwamoto <info@tokiwakanpou.com>
2020-04-03 11:20:57 +0900

    gesture segue

---
+++ SlideshowApp/ResultViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ResultViewController.swift
+//  SlideshowApp
+//
+//  Created by 岩本知章 on 2020/04/02.
+//  Copyright © 2020 tomoaki.iwamoto. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -9,12 +9,70 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var imageLabel: UIImageView!
+    @IBOutlet weak var nextButton: UIButton!
+    @IBOutlet weak var backButton: UIButton!
+    @IBOutlet weak var startStopButton: UIButton!
+    
+    
+    var imageDisplayArray = ["image1.jpg", "image2.jpg", "image3.jpg"]
+    var imageNo = 0
+    var image = UIImage(named: "image1.jpg")
+    var timer: Timer!
+    @IBAction func backAction(_ sender: Any) {
+        imageNo -= 1
+        display()
+    }
+    
+    @IBAction func nextAction(_ sender: Any) {
+        imageNo += 1
+        display()
+    }
+    @IBAction func startStopAction(_ sender: Any) {
+        if self.timer == nil{
+            self.timer = Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(updateTimer(_:)), userInfo: nil, repeats: true)
+            backButton.isEnabled = false
+            nextButton.isEnabled = false
+            startStopButton.setTitle("停止", for: .normal)
+        } else if self.timer != nil {
+            self.timer.invalidate()
+            self.timer = nil
+            backButton.isEnabled = true
+            nextButton.isEnabled = true
+            startStopButton.setTitle("再生", for: .normal)
+            
+        }
+       
+    }
+    
+    @objc func updateTimer(_ timer:Timer){
+        imageNo += 1
+        display()
+    }
+    
+    func display(){
+        if imageNo > 2 {
+            imageNo = 0
+        } else if imageNo < 0 {
+            imageNo = 2
+        }
+        let name = imageDisplayArray[imageNo]
+        let image = UIImage(named: name)
+        imageLabel.image = image
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        imageLabel.image = image
     }
-
+    @IBAction func onTapImage(_ sender: Any) {
+        performSegue(withIdentifier: "result", sender: nil)
+    }
+    
+    @IBAction func unwind(_ segue: UIStoryboardSegue){
+        
+    }
 
 }
 
